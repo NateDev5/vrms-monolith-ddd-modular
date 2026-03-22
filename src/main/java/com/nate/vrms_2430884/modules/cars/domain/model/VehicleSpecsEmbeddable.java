6811e5cb51a7731932e7ebaf312ab8cc0bfdc3e7
NateDev5 <nate.bes@protonmail.com>
2026-03-21 20:37:19 -0400

    AS5

---
+++ src/main/java/com/nate/vrms_2430884/modules/cars/domain/model/VehicleSpecsEmbeddable.java
@@ -0,0 +1,21 @@
+package com.nate.vrms_2430884.modules.cars.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class VehicleSpecsEmbeddable {
+    @Column(name = "make", nullable = false)
+    public String make;
+    @Column(name = "model", nullable = false)
+    public String model;
+    @Column(name = "vehicle_year", nullable = false)
+    public int year;
+    protected VehicleSpecsEmbeddable() {
+    }
+    public VehicleSpecsEmbeddable(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
--- src/main/java/com/nate/vrms_2430884/modules/cars/infrastructure/persistence/VehicleJpaEntity.java
+++ src/main/java/com/nate/vrms_2430884/modules/cars/infrastructure/persistence/VehicleJpaEntity.java
@@ -1,5 +1,6 @@
 package com.nate.vrms_2430884.modules.cars.infrastructure.persistence;
 
+import com.nate.vrms_2430884.modules.cars.domain.model.VehicleSpecsEmbeddable;
 import jakarta.persistence.*;
 
 @Entity
@@ -7,14 +8,13 @@ import jakarta.persistence.*;
 public class VehicleJpaEntity {
     @Id
     public String id;
+
     @Column(nullable = false, unique = true)
     public String vin;
-    @Column(nullable = false)
-    public String make;
-    @Column(nullable = false)
-    public String model;
-    @Column(nullable = false)
-    public int vehicle_year;
+
+    @Embedded
+    public VehicleSpecsEmbeddable specs;
+
     @Column(nullable = false)
     public String status;
 }
